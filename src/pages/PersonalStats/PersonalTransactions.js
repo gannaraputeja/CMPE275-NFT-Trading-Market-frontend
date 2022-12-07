841f8504877cdcd90bd4ee85971734e5bb3b5e1c
sarat458 <srthkmr491@gmail.com>
2022-12-06 21:00:48 -0800

    designed data flow in personal transactions page

--- src/pages/PersonalStats/PersonalTransactions.js
+++ src/pages/PersonalStats/PersonalTransactions.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable react/jsx-filename-extension */
 import { Box, Button, Container } from '@mui/material';
 import React from 'react';
@@ -5,9 +6,109 @@ import PeriodFilter from '../Dashboard/PeriodFilter';
 import CurrencyFilter from '../Dashboard/CurrencyFilter';
 import Transactions from './Transactions';
 
+const transactions = [
+  {
+    id: 1,
+    avatar: 'https://firebasestorage.googleapis.com/v0/b/nft-trading-market-7e98b.appspot.com/o/images%2FNFT_marketplace.ico0531b430-0640-49a8-87a6-a9edd4c9aab5?alt=media&token=b53d127c-b7a3-4a34-b6fa-2b543451ab79',
+    nftName: 'Photos',
+    nftType: 'Art',
+    buyer: 'nkf_90',
+    seller: 'srth098',
+    listingType: 'priced',
+    price: 100,
+    currency: 'BTC',
+    transactionDate: '12-06-2022 12:08:10PST',
+  },
+  {
+    id: 2,
+    avatar: 'https://firebasestorage.googleapis.com/v0/b/nft-trading-market-7e98b.appspot.com/o/images%2FNFT_marketplace.ico0531b430-0640-49a8-87a6-a9edd4c9aab5?alt=media&token=b53d127c-b7a3-4a34-b6fa-2b543451ab79',
+    nftName: 'Photos',
+    nftType: 'Art',
+    buyer: 'nkf_90',
+    seller: 'srth098',
+    listingType: 'priced',
+    price: 100,
+    currency: 'BTC',
+    transactionDate: '12-06-2022 12:08:10PST',
+  },
+  {
+    id: 3,
+    avatar: 'https://firebasestorage.googleapis.com/v0/b/nft-trading-market-7e98b.appspot.com/o/images%2FNFT_marketplace.ico0531b430-0640-49a8-87a6-a9edd4c9aab5?alt=media&token=b53d127c-b7a3-4a34-b6fa-2b543451ab79',
+    nftName: 'Photos',
+    nftType: 'Art',
+    buyer: 'nkf_90',
+    seller: 'srth098',
+    listingType: 'auctioned',
+    price: 100,
+    currency: 'BTC',
+    transactionDate: '12-06-2022 12:08:10PST',
+  },
+  {
+    id: 4,
+    avatar: 'https://firebasestorage.googleapis.com/v0/b/nft-trading-market-7e98b.appspot.com/o/images%2FNFT_marketplace.ico0531b430-0640-49a8-87a6-a9edd4c9aab5?alt=media&token=b53d127c-b7a3-4a34-b6fa-2b543451ab79',
+    nftName: 'Photos',
+    nftType: 'Art',
+    buyer: 'nkf_90',
+    seller: 'srth098',
+    listingType: 'auctioned',
+    price: 100,
+    currency: 'BTC',
+    transactionDate: '12-06-2022 12:08:10PST',
+  },
+  {
+    id: 5,
+    avatar: 'https://firebasestorage.googleapis.com/v0/b/nft-trading-market-7e98b.appspot.com/o/images%2FNFT_marketplace.ico0531b430-0640-49a8-87a6-a9edd4c9aab5?alt=media&token=b53d127c-b7a3-4a34-b6fa-2b543451ab79',
+    nftName: 'Photos',
+    nftType: 'Art',
+    buyer: 'nkf_90',
+    seller: 'srth098',
+    listingType: 'priced',
+    price: 100,
+    currency: 'BTC',
+    transactionDate: '12-06-2022 12:08:10PST',
+  },
+  {
+    id: 6,
+    avatar: 'https://firebasestorage.googleapis.com/v0/b/nft-trading-market-7e98b.appspot.com/o/images%2FNFT_marketplace.ico0531b430-0640-49a8-87a6-a9edd4c9aab5?alt=media&token=b53d127c-b7a3-4a34-b6fa-2b543451ab79',
+    nftName: 'Photos',
+    nftType: 'Art',
+    buyer: 'nkf_90',
+    seller: 'srth098',
+    listingType: 'priced',
+    price: 100,
+    currency: 'BTC',
+    transactionDate: '12-06-2022 12:08:10PST',
+  },
+  {
+    id: 7,
+    avatar: 'https://firebasestorage.googleapis.com/v0/b/nft-trading-market-7e98b.appspot.com/o/images%2FNFT_marketplace.ico0531b430-0640-49a8-87a6-a9edd4c9aab5?alt=media&token=b53d127c-b7a3-4a34-b6fa-2b543451ab79',
+    nftName: 'Photos',
+    nftType: 'Art',
+    buyer: 'nkf_90',
+    seller: 'srth098',
+    listingType: 'auctioned',
+    price: 100,
+    currency: 'BTC',
+    transactionDate: '12-06-2022 12:08:10PST',
+  },
+  {
+    id: 8,
+    avatar: 'https://firebasestorage.googleapis.com/v0/b/nft-trading-market-7e98b.appspot.com/o/images%2FNFT_marketplace.ico0531b430-0640-49a8-87a6-a9edd4c9aab5?alt=media&token=b53d127c-b7a3-4a34-b6fa-2b543451ab79',
+    nftName: 'Photos',
+    nftType: 'Art',
+    buyer: 'nkf_90',
+    seller: 'srth098',
+    listingType: 'priced',
+    price: 100,
+    currency: 'BTC',
+    transactionDate: '12-06-2022 12:08:10PST',
+  },
+];
+
 function SystemTransactionStats() {
   const [period, setPeriod] = React.useState('');
   const [currency, setCurrency] = React.useState('');
+  const [transactionsData, setTransactionsData] = React.useState([]);
 
   const handlePeriodState = (value) => {
     setPeriod(value);
@@ -19,15 +120,20 @@ function SystemTransactionStats() {
 
   const handleSubmit = () => {
     console.log(period, currency);
+    const filterData = [...transactions];
+    setTransactionsData(filterData.filter((row) => row.transactionDate >= (Date.now() - period)
+    && row.currency === currency));
   };
 
   const handleReset = () => {
     setPeriod('');
     setCurrency('');
+    setTransactionsData(transactions);
   };
 
   React.useEffect(() => {
     // backend call to fetch all the results.
+    setTransactionsData(transactions);
   }, []);
   return (
     <Container>
@@ -56,7 +162,7 @@ function SystemTransactionStats() {
       </Box>
       <Container>
         <Box sx={{ display: 'flex', gridTemplateRows: 'repeat(4, 1fr)', justifyContent: 'center' }}>
-          <Transactions />
+          <Transactions transactionsData={transactionsData} />
         </Box>
       </Container>
     </Container>
--- src/pages/PersonalStats/Transactions.js
+++ src/pages/PersonalStats/Transactions.js
@@ -1,79 +1,85 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable consistent-return */
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-filename-extension */
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
+import { Avatar, Chip } from '@mui/material';
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
   {
-    field: 'firstName',
-    headerName: 'First name',
+    field: 'avatar',
+    headerName: 'Image',
+    width: 100,
+    renderCell: (params) => (
+      <div style={{ padding: 5, justifyContent: 'center' }}>
+        <img alt="#" src={params.value} height="50px" width="50px" />
+      </div>
+    ),
+  },
+  { field: 'nftName', headerName: 'NFT Name', width: 100 },
+  {
+    field: 'nftType',
+    headerName: 'NFT Type',
     width: 150,
     editable: true,
   },
   {
-    field: 'lastName',
-    headerName: 'Last name',
-    width: 150,
+    field: 'buyer',
+    headerName: 'Buyer',
+    width: 80,
     editable: true,
   },
   {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 110,
+    field: 'seller',
+    headerName: 'Seller',
+    width: 80,
     editable: true,
   },
   {
-    field: 'fullName',
-    headerName: 'Full name',
+    field: 'listingType',
+    headerName: 'Listing Type',
     description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) => `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    width: 120,
+    renderCell: (params) => (
+      <Chip label={params.value === 'priced' ? 'Priced' : 'Auctioned'} color={params.value === 'priced' ? 'info' : 'success'} />
+    ),
+  },
+  {
+    field: 'price',
+    headerName: 'Price',
+    type: 'number',
+    width: 80,
+  },
+  {
+    field: 'currency',
+    headerName: 'Currency',
+    type: 'string',
+    width: 80,
+  },
+  {
+    field: 'transactionDate',
+    headerName: 'Transaction Date',
+    type: 'Date',
+    width: 200,
   },
 ];
 
-const rows = [
-  {
-    id: 1, lastName: 'Snow', firstName: 'Jon', age: 35,
-  },
-  {
-    id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42,
-  },
-  {
-    id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45,
-  },
-  {
-    id: 4, lastName: 'Stark', firstName: 'Arya', age: 16,
-  },
-  {
-    id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null,
-  },
-  {
-    id: 6, lastName: 'Melisandre', firstName: null, age: 150,
-  },
-  {
-    id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44,
-  },
-  {
-    id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36,
-  },
-  {
-    id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65,
-  },
-];
-
-export default function DataGridDemo() {
+export default function DataGridDemo({ transactionsData }) {
   return (
     <Box sx={{ height: 400, width: '100%' }}>
       <DataGrid
-        rows={rows}
+        rows={transactionsData}
         columns={columns}
         pageSize={5}
         rowsPerPageOptions={[5]}
         checkboxSelection
         disableSelectionOnClick
+        disableColumnFilter
+        disableColumnSelector
+        disableColumnMenu
+        onColumnHeaderClick={() => console.log('Header clicked')}
         experimentalFeatures={{ newEditingApi: true }}
       />
     </Box>
